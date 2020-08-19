d1008a3a6ec73839d9fbedcc3898a70f51694f1b
Vladimir Feldman <wfel@narod.ru>
2020-08-20 00:38:04 +0400

    Static

--- clubbase/urls.py
+++ clubbase/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("mainpage.urls")),
     path('clubs/', include("clublist.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
